0f969e6da7b3d512763ae7c7cba2b1cbeed9202e
j5m97 <wlstmdals12@gmail.com>
2025-10-04 16:44:11 +0900

    Day 2025_10_04

--- baekjoon/Day04/boj_1181.java
+++ baekjoon/Day04/boj_1181.java
@@ -5,6 +5,26 @@ import java.io.*;
 
 public class boj_1181 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(br.readLine());
+
+        List<String> words = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+
+            if (words.contains(str))
+                continue;
+
+            words.add(str);
+        }
+
+        Collections.sort(words, (o1, o2) -> o1.length() == o2.length() ? o1.compareTo(o2) : o1.length() - o2.length());
+
+        for (String s : words) {
+            System.out.println(s);
+        }
+
+        br.close();
     }
 }
